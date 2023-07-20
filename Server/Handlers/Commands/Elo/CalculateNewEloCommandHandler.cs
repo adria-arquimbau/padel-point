ebc21c905709b09fc733f9dbb01e28fc4c042ba0
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-07-20 18:03:32 +0200

    "Refine Elo calculation and add Elo change history to player model
    The Elo rating calculation has been updated in CalculateNewEloCommandHandler. The selection of players is now based on their team assignment rather than the order in which they are listed. The actual score now also takes into account the set score difference. This makes the Elo calculation more accurate as it better reflects a team's performance in a match.
    
    An 'EloChange' property has been added to the player model to track the changes in a player's Elo rating after each match. This provides a more detailed history of a player's performance over time.
    
    The Documentation.razor page has been updated to reflect these changes in the Elo rating system's explanation."

--- Server/Handlers/Commands/Elo/CalculateNewEloCommandHandler.cs
+++ Server/Handlers/Commands/Elo/CalculateNewEloCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using EventsManager.Server.Data;
 using EventsManager.Server.Models;
+using EventsManager.Shared;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,60 +24,71 @@ public class CalculateNewEloCommandHandler : IRequestHandler<CalculateNewEloComm
         .Include(x => x.Sets)
         .SingleAsync(cancellationToken: cancellationToken);
 
-    // Assume team1 is the first two players and team2 is the second two
-    var players = match.MatchPlayers.ToList();
-    var team1 = new [] { players[0].Player, players[1].Player };
-    var team2 = new [] { players[2].Player, players[3].Player };
+        // Assume team1 is the first two players and team2 is the second two
+        var playersTeamOne = match.MatchPlayers.Where(x => x.Team == Team.Team1).ToList();
+        var playersTeamTwo = match.MatchPlayers.Where(x => x.Team == Team.Team2).ToList();
+        var team1 = new [] { playersTeamOne[0].Player, playersTeamOne[1].Player };
+        var team2 = new [] { playersTeamTwo[0].Player, playersTeamTwo[1].Player };
 
-    // Assume team1 won if they have a higher total score than team2
-    var team1TotalScore = match.Sets.Sum(set => set.Team1Score);
-    var team2TotalScore = match.Sets.Sum(set => set.Team2Score);
-    var team1Won = team1TotalScore > team2TotalScore;
+        // Assume team1 won if they have a higher total score than team2
+        var team1TotalScore = match.Sets.Sum(set => set.Team1Score);
+        var team2TotalScore = match.Sets.Sum(set => set.Team2Score);
+        var team1Won = team1TotalScore > team2TotalScore;
 
-    const int k = 32;
+        const int k = 32;
 
-    foreach (var player in team1)
-    {
-        var otherTeamElo = team2.Average(p => p.Elo);
-
-        var expectedScore = 1.0 / (1.0 + Math.Pow(10, (otherTeamElo - player.Elo) / 400.0));
-        var actualScore = team1Won ? 1.0 : 0.0;
-
-        var newElo = player.Elo + (int)(k * (actualScore - expectedScore));
-        
-        player.EloHistories.Add(new EloHistory
+        foreach (var player in team1)
         {
-            PreviousElo = player.Elo,
-            CurrentElo = newElo,
-            ChangeDate = DateTime.UtcNow,
-            MatchId = match.Id,
-            PlayerId = player.Id
-        });
+            // Average Elo rating of the opposing team
+            var otherTeamElo = team2.Average(p => p.Elo);
 
-        player.Elo = newElo;
-    }
+            // Expected score, based on the player's current Elo rating and the average Elo rating of the opposing team
+            var expectedScore = 1.0 / (1.0 + Math.Pow(10, (otherTeamElo - player.Elo) / 400.0));
 
-    foreach (var player in team2)
-    {
-        var otherTeamElo = team1.Average(p => p.Elo);
+            // Actual score, based on whether the player's team won and by how much
+            var actualScore = team1Won ? 1.0 + (team1TotalScore - team2TotalScore) / 10.0 : 0.0;
 
-        var expectedScore = 1.0 / (1.0 + Math.Pow(10, (otherTeamElo - player.Elo) / 400.0));
-        var actualScore = team1Won ? 0.0 : 1.0;
+            // New Elo rating, based on the player's current Elo rating, the expected score, the actual score, and the K-factor
+            var newElo = player.Elo + (int)(k * (actualScore - expectedScore));
 
-        var newElo = player.Elo + (int)(k * (actualScore - expectedScore));
+            // Save the Elo rating change in the player's history
+            player.EloHistories.Add(new EloHistory
+            {
+                PreviousElo = player.Elo,
+                CurrentElo = newElo,
+                EloChange = newElo - player.Elo,
+                ChangeDate = DateTime.UtcNow,
+                MatchId = match.Id,
+                PlayerId = player.Id
+            });
 
-        player.EloHistories.Add(new EloHistory
+            // Update the player's Elo rating
+            player.Elo = newElo;
+        }
+
+        // Repeat the same process for team 2
+        foreach (var player in team2)
         {
-            PreviousElo = player.Elo,
-            CurrentElo = newElo,
-            ChangeDate = DateTime.UtcNow,
-            MatchId = match.Id,
-            PlayerId = player.Id
-        });
+            var otherTeamElo = team1.Average(p => p.Elo);
+            var expectedScore = 1.0 / (1.0 + Math.Pow(10, (otherTeamElo - player.Elo) / 400.0));
+            var actualScore = team1Won ? 0.0 : 1.0 + (team2TotalScore - team1TotalScore) / 10.0;
+            var newElo = player.Elo + (int)(k * (actualScore - expectedScore));
 
-        player.Elo = newElo;
-    }
+            player.EloHistories.Add(new EloHistory
+            {
+                PreviousElo = player.Elo,
+                CurrentElo = newElo,
+                EloChange = newElo - player.Elo,
+                ChangeDate = DateTime.UtcNow,
+                MatchId = match.Id,
+                PlayerId = player.Id
+            });
 
-    await _context.SaveChangesAsync(cancellationToken);
+            player.Elo = newElo;
+        }
+
+        // Save changes to the database
+        await _context.SaveChangesAsync(cancellationToken);
+
     }
 }
--- Server/Models/Player.cs
+++ Server/Models/Player.cs
@@ -17,6 +17,7 @@ public class EloHistory
     public Guid Id { get; set; }
     public int PreviousElo { get; set; }
     public int CurrentElo { get; set; }
+    public int EloChange { get; set; }
     public DateTime ChangeDate { get; set; }
     public Guid MatchId { get; set; }
     public Match Match { get; set; }
