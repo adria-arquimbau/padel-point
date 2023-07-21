0a8fb59e30943ae056ffa3d5d170e2a44410f25b
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-07-21 16:14:07 +0200

    Add UserName field to ExternalLogin
    Updated ExternalLogin.cshtml.cs and ExternalLogin.cshtml to include a UserName field. ExternalLogin is now required to have a UserName input upon registration. This change will enhance the uniqueness of login process and improve data integrity. Prior to this, users were linked by emails only which could be problematic.

--- Server/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -80,6 +80,9 @@ namespace EventsManager.Server.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             public string Email { get; set; }
+            [Required]
+            [MaxLength(14)]
+            public string UserName { get; set; }
         }
         
         public IActionResult OnGet() => RedirectToPage("./Login");
@@ -149,7 +152,7 @@ namespace EventsManager.Server.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.UserName, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
 
                 var result = await _userManager.CreateAsync(user);
