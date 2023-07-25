b807122db85d6513512f30cdd22db32105dc6e53
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-07-25 19:10:42 +0200

    Remove associated EloHistories before deleting user
    Modified the `DeleteUserCommandHandler.cs` server handler to delete the EloHistories associated with a user before deleting the user itself. This is to ensure referential integrity and avoid orphaned records in the `EloHistories` table. The user's `Player` and related `EloHistories` are initially fetched from the database, then the associated `EloHistories` records are removed before deleting the user.

--- Server/Handlers/Commands/User/DeleteUser/DeleteUserCommandHandler.cs
+++ Server/Handlers/Commands/User/DeleteUser/DeleteUserCommandHandler.cs
@@ -15,6 +15,13 @@ public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommandRequest
     
     public async Task Handle(DeleteUserCommandRequest request, CancellationToken cancellationToken)
     {
+        var user = await _dbContext.Users
+            .Include(x => x.Player)
+            .ThenInclude(x => x.EloHistories)
+            .SingleAsync(x => x.Id == request.UserId, cancellationToken: cancellationToken);
+        
+        _dbContext.EloHistories.RemoveRange(user.Player.EloHistories);
+        await _dbContext.SaveChangesAsync(cancellationToken);
         await _dbContext.Users
             .Where(x => x.Id == request.UserId)
             .ExecuteDeleteAsync(cancellationToken: cancellationToken);
