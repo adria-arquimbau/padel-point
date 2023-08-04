bc7d6c2bb730c12216a96a9b882ca92c185e11b7
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-08-04 13:16:12 +0200

    Remove redundant CSS link and enhance user authentication
    This commit removes a redundant CSS link in the index.html file that was needlessly increasing page load
    time. Changes have also been made in MainLayout.razor to improve user authentication by checking if the
    user is authenticated and fetching necessary information during initialization. This is done by injecting
    AuthenticationStateProvider, IToastService, and HttpClient dependencies and using them in the OnInitializedAsync
    method.
    
    In addition, a new file named AnnouncementController.cs, which checks whether the initial level is done or not,
    is added to improve the gaming progress tracking. Lastly, adjustments are made to Notifications.razor styling,
    and a minor clean-up was done in NavMenu.razor.

---
+++ Server/Controllers/AnnouncementController.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Claims;
+using EventsManager.Server.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsManager.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnnouncementController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public AnnouncementController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    [HttpGet("initial-level-done")]
+    [Authorize(Roles = "User")]
+    public async Task<IActionResult> IsInitialLevelDone(CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        var done = await _dbContext.Player
+            .Where(x => x.UserId == userId)
+            .Select(x => x.Announcements.InitialLevelFormDone)
+            .SingleAsync(cancellationToken: cancellationToken);
+        
+        return Ok(done);
+    }
+    
+    [HttpPost("initial-level-done")]
+    [Authorize(Roles = "User")]
+    public async Task<IActionResult> InitialLevelDone(CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        var announcements = await _dbContext.Announcements
+            .Where(x => x.Player.UserId == userId)
+            .SingleAsync(cancellationToken: cancellationToken);
+        
+        announcements.InitialLevelFormDone = true;
+        
+        await _dbContext.SaveChangesAsync(cancellationToken);
+        
+        return Ok();
+    }
+}
