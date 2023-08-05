f1c67df24e1559dd416689fe61dc3f9a269f3ba4
Adria Arquimbau <adria.arquimbau@gmail.com>
2023-08-06 00:26:28 +0200

    Update player's initial skill calibration
    Change is made in the InitialPlayerSkillCalibrationCommandHandler to modify how other racket sports level is set. Now if a player hasn't played any other racket sports before, their initial skill level for those sports are set to 'None' by default. This removes potential bias and confusion when ranking players based on their skill level.

--- Server/Handlers/Commands/Elo/InitialPlayerSkillCalibrationCommandHandler/InitialPlayerSkillCalibrationCommandHandler.cs
+++ Server/Handlers/Commands/Elo/InitialPlayerSkillCalibrationCommandHandler/InitialPlayerSkillCalibrationCommandHandler.cs
@@ -47,7 +47,7 @@ public class InitialPlayerSkillCalibrationCommandHandler : IRequestHandler<Initi
         player.InitialLevelForm = new InitialLevelForm
         {
             OtherRacketSportsYearsPlaying = request.Request.OtherRacketSportsYearsPlaying,
-            OtherRacketSportsLevel = request.Request.OtherRacketSportsLevel,
+            OtherRacketSportsLevel = request.Request.PlayedOtherRacketSportsBefore ? request.Request.OtherRacketSportsLevel : OtherRacketSportsLevel.None,
             PlayedOtherRacketSportsBefore = request.Request.PlayedOtherRacketSportsBefore,
             SelfAssessedPadelSkillLevel = request.Request.SelfAssessedPadelSkillLevel,
             YearsPlayingPadel = request.Request.YearsPlayingPadel,
