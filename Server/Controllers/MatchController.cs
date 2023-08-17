3d8686cb4274a33ce8263425910f979b90760797
Adria Arquimbau <adria.arquimbau@gmail.com>
2023-08-17 15:40:28 +0200

    Fix: Removed creator's ability to modify match scores
    In the MatchController, the validation for who can set or remove scores previously allowed both the players and the creator of the match to perform these actions. This commit removes that permission from the match creator, now only the participating players can alter the match scores. This change ensures the behavior aligns with the game rules.

--- Server/Controllers/MatchController.cs
+++ Server/Controllers/MatchController.cs
@@ -335,7 +335,7 @@ public class MatchController : ControllerBase
                 .ThenInclude(x => x.Player)
             .SingleAsync(cancellationToken: cancellationToken);
 
-        if (match.MatchPlayers.All(x => x.Player.UserId != userId) && match.Creator.UserId != userId)
+        if (match.MatchPlayers.All(x => x.Player.UserId != userId))
         {
             return Conflict("Only players can set score.");
         }
@@ -394,7 +394,7 @@ public class MatchController : ControllerBase
             .Include(x => x.Sets)
             .SingleAsync(cancellationToken: cancellationToken);
 
-        if (match.MatchPlayers.All(x => x.Player.UserId != userId) && match.Creator.UserId != userId)
+        if (match.MatchPlayers.All(x => x.Player.UserId != userId))
         {
             return Conflict("Only players can remove score.");
         }
