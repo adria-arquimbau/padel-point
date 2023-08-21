71c2e8c5eb557b7e37cc8e222580e6ebf6a60abe
Adria Arquimbau <a.arquimbau@palfinger.com>
2023-08-21 22:03:15 +0200

    Remove round-robin feature from Tournament model
    The round-robin related properties and classes were removed from the Tournament model. This was done as the round-robin feature is no longer part of the tournament structure and was causing confusion and complexity without adding value to the application. All related aspects, such as phases, rounds and matches associated with the round-robin format, were discarded.

--- Server/Models/Tournament.cs
+++ Server/Models/Tournament.cs
@@ -17,30 +17,4 @@ public class Tournament
     public decimal Price { get; set; }
     public bool RegistrationOpen { get; set; }
     public bool ShowBrackets { get; set; }
-    public bool RoundRobinEnabled { get; set; }
-    public RoundRobinPhase? RoundRobinPhase { get; set; }
-}
-    
-public class RoundRobinPhase    
-{
-    public Guid Id { get; set; }
-    public ICollection<Round> Rounds { get; set; } = new List<Round>();
-    public int MaxRounds { get; set; }
-}
-
-public class Round
-{
-    public Guid Id { get; set; }
-    public int Number { get; set; } 
-    public TournamentMatch Match { get; set; }
-}   
-
-public class TournamentMatch
-{
-    public Guid Id { get; set; }
-    public Guid Team1Id { get; set; }
-    public Couple Team1 { get; set; }
-    public Guid Team2Id { get; set; }
-    public Couple Team2 { get; set; }
-    public List<Set> Score { get; set; }
 }
