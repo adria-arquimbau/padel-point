6c14e3db43420ba75f3cbaee8704814d63d3126b
Adria Arquimbau <adria.arquimbau@gmail.com>
2023-09-05 09:59:59 +0200

    refact

---
+++ Server/Data/Migrations/20230905075442_AddTournamentUri.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventsManager.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTournamentUri : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUri",
+                table: "Tournament",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUri",
+                table: "Tournament");
+        }
+    }
+}
